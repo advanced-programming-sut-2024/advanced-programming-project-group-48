9f5d08d9eeb28921523e5d7814895924c7415f9d
ghost <amiralibeigi1@gmail.com>
2024-06-20 18:17:19 +0330

    mohsen hastam

--- Gwent/src/main/java/Main.java
+++ Gwent/src/main/java/Main.java
@@ -1,9 +1,10 @@
+import model.User;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("salma baraye commit ezafe shodam");
-        int ali;
-        int mamad;
+        User user;
     }
 
 }
