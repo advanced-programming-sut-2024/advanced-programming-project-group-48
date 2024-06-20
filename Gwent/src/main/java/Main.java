d2c2d8ac73ea890b944a875f4c48a1368c66445a
ghost <amiralibeigi1@gmail.com>
2024-06-20 18:06:56 +0330

    amirali

--- Gwent/src/main/java/Main.java
+++ Gwent/src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("salma baraye commit ezafe shodam");
+        int ali;
     }
 
 }
